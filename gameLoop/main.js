77a7fafa9b53f4bf04e01569656abc90e205302e
nurfuis <cramer.ross@gmail.com>
2024-08-03 18:27:06 -0500

    pruning

--- gameLoop/main.js
+++ gameLoop/main.js
@@ -1,9 +1,7 @@
-const debug = true;
-
 const windowWidth = window.innerWidth;
 const windowHeight = window.innerHeight;
 
-import { events } from "./src/Events.js";
+import { gameParams } from "./constants.js";
 import { Vector2 } from "./src/Vector2.js";
 import { GameObject } from "./src/GameObject.js";
 import { GameLoop } from "./src/GameLoop.js";
@@ -13,53 +11,24 @@ const gameCanvasMain = createGameCanvasMain();
 const ctx = gameCanvasMain.getContext("2d");
 
 
-let main;
+const main = new GameObject({ position: new Vector2(0, 0) });
+
 
 const update = (delta) => {
-
   main.stepEntry(delta, main);
-  console.log(gameLoop.fps)
+  // console.log("Update...")
 };
 const draw = () => {
   ctx.clearRect(0, 0, windowWidth, windowHeight);
 
   main.draw(ctx, 0, 0);
+  // console.log("Draw...")
 
 };
 
+
 const gameLoop = new GameLoop(update, draw);
-gameLoop.name = "mainLoop";
 
-function sortChildren(array) {
-  array.children.sort((a, b) => {
-    const aQuadrant =
-      a.position.x >= 0
-        ? a.position.y >= 0
-          ? 1
-          : 4
-        : a.position.y >= 0
-          ? 2
-          : 3;
-    const bQuadrant =
-      b.position.x >= 0
-        ? b.position.y >= 0
-          ? 1
-          : 4
-        : a.position.y >= 0
-          ? 2
-          : 3;
-
-    if (aQuadrant !== bQuadrant) {
-      return aQuadrant - bQuadrant;
-    } else {
-      if (a.position.y !== b.position.y) {
-        return a.position.y - b.position.y;
-      } else {
-        return a.position.x - b.position.x;
-      }
-    }
-  });
-}
 
 function createGameCanvasMain() {
   const gameCanvasMain = document.createElement("canvas");
@@ -84,55 +53,11 @@ function createGameWrapper() {
   body[0].appendChild(gameWrapper);
   return gameWrapper;
 }
-function createStartButton() {
-  const body = document.getElementsByTagName("body");
 
-  const startContainer = document.createElement("div");
-  startContainer.classList.add("container");
-
-  const startChild = document.createElement("div");
-  startContainer.appendChild(startChild);
-
-  const startButton = document.createElement("button");
-  startButton.textContent = "Start";
-  startButton.id = "start";
-  startButton.addEventListener("click", () => {
-    startMain();
-    startContainer.remove();
-    console.log("Start!");
-  });
-
-  startChild.appendChild(startButton);
-
-  body[0].appendChild(startContainer);
-  return startContainer;
-}
-
-async function startMain() {
-  main = new GameObject({ position: new Vector2(0, 0) });
-
-  if (debug) console.log(main);
-
-  gameLoop.start();
-
-  gameCanvasMain.addEventListener("mousedown", (e) => {
-    const rect = gameCanvasMain.getBoundingClientRect();
-
-    const clickX = Math.round(e.clientX - rect.left);
-    const clickY = Math.round(e.clientY - rect.top);
-
-    const clickPos = new Vector2(clickX, clickY);
-
-    events.emit("CLICK", clickPos )
-
-    console.log(
-      "Click: ", clickPos
-    );
-  });
-}
 
 window.onload = function () {
-  if (!AUTO_START) createStartButton();
+  gameLoop.start();
+  console.log("Starting!")
 };
 
 
